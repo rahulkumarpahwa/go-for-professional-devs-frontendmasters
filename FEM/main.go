20776e220c3006ede413add99acedcb639356ba7
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2026-03-31 06:46:35 +0530

    L19: Creating an HTTP Server

--- FEM/main.go
+++ FEM/main.go
@@ -1,6 +1,12 @@
 package main
 
-import "github.com/rahulkumarpahwa/femProject/internal/app"
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/rahulkumarpahwa/femProject/internal/app"
+)
 
 func main() {
 	app, err := app.NewApplication()
@@ -9,4 +15,23 @@ func main() {
 	}
 
 	app.Logger.Println("App Started Running!")
+
+	http.HandleFunc("/health", HealthCheck)
+
+	Server := &http.Server{
+		Addr:         ":8080",
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	err = Server.ListenAndServe()
+	if err != nil {
+		app.Logger.Fatal(err)
+	}
+
+}
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Status is Available!")
 }
