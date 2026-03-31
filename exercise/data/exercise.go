4b3e0abe7b52439d884ea93ed7023665212e68f5
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2026-03-31 06:09:24 +0530

    Refactor DropItem and UseItem methods for improved inventory management

--- exercise/data/exercise.go
+++ exercise/data/exercise.go
@@ -19,25 +19,28 @@ func (p *Player) PickUpItem(item Item) {
 }
 
 func (p *Player) DropItem(itemName string) {
-	var newInventory []Item
-	for _, i := range p.Inventory {
-		if i.Name != itemName {
-			newInventory = append(newInventory, i)
+	for i, item := range p.Inventory {
+		if item.Name == itemName {
+			p.Inventory = append(p.Inventory[:i], p.Inventory[i+1:]...)
+			fmt.Printf("%s has Dropped %s/n", p.Name, itemName)
+			return
 		}
 	}
-	fmt.Printf("%s has Dropped %s/n", p.Name, itemName)
-	p.Inventory = newInventory
+	fmt.Printf("%s does not has %s in inventory./n", p.Name, itemName)
 }
 
 // Use an item (if potion, remove it after use)
 func (p *Player) UseItem(itemName string) {
-	// TODO: Implement function to use an item
-	var newInventory []Item
-	for _, i := range p.Inventory {
-		if i.Name != itemName {
-			newInventory = append(newInventory, i)
+	for i, item := range p.Inventory {
+		if item.Name == itemName {
+			if item.Type == "potion" {
+				fmt.Printf("%s has used %s and feels rejuninated!/n", p.Name, itemName)
+				p.Inventory = append(p.Inventory[:i], p.Inventory[i+1:]...)
+			} else {
+				fmt.Printf("%s uses %s/n", p.Name, itemName)
+			}
+			return
 		}
 	}
-	fmt.Printf("%s has uses %s/n", p.Name, itemName)
-	p.Inventory = newInventory
+	fmt.Printf("%s does not has %s in inventory./n", p.Name, itemName)
 }
