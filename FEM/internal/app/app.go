cf658b6ba769b3fdeca5fea031a4ebc80054c134
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2026-04-02 08:28:22 +0530

    L26: Running Goose Migrations

--- FEM/internal/app/app.go
+++ FEM/internal/app/app.go
@@ -9,27 +9,33 @@ import (
 
 	"github.com/rahulkumarpahwa/femProject/internal/api"
 	"github.com/rahulkumarpahwa/femProject/internal/store"
+	"github.com/rahulkumarpahwa/femProject/migrations"
 )
 
 type Application struct {
 	WorkoutHandler *api.WorkoutHandler
 	Logger         *log.Logger
-	DB  *sql.DB
+	DB             *sql.DB
 }
 
 func NewApplication() (*Application, error) {
-	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
-
 	// Our stores will go here
 	pgDB, err := store.Open()
 	if err != nil {
 		return nil, err
 	}
 
+	err = store.MigrateFS(pgDB, migrations.FS ,".")
+	if err !=nil{
+		panic(err)
+	}
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
 	// our handlers will go here
 	workoutHandler := api.NewWorkoutHandler(pgDB, logger)
 
-	app := &Application{Logger: logger, WorkoutHandler: workoutHandler, DB:  pgDB}
+	app := &Application{Logger: logger, WorkoutHandler: workoutHandler, DB: pgDB}
 	return app, nil
 }
 
