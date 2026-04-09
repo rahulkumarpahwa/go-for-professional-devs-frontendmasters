65eda6b0b32dea296ea23e941bdb51b6e056730a
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2026-04-09 14:39:28 +0530

    L29: CreateWorkout Query

--- FEM/internal/store/workout_store.go
+++ FEM/internal/store/workout_store.go
@@ -9,17 +9,18 @@ import (
 type Workout struct {
 	ID int `json:"id"`
 	//add the user id later here.
-	Title           string    `json:"title"`
-	Description     string    `json:"description"`
-	DurationMinutes int       `json:"duration_minutes"`
-	CaloriesBurned  int       `json:"calories_burned"`
-	CreatedAt       time.Time `json:"created_at"`
-	UpdatedAt       time.Time `json:"updated_at"`
+	Title           string         `json:"title"`
+	Description     string         `json:"description"`
+	DurationMinutes int            `json:"duration_minutes"`
+	CaloriesBurned  int            `json:"calories_burned"`
+	CreatedAt       time.Time      `json:"created_at"`
+	UpdatedAt       time.Time      `json:"updated_at"`
+	Entries         []WorkoutEntry `json:"entries"`
 }
 
 type WorkoutEntry struct {
-	ID int `json:"id"`
-	// WorkoutId       int       `json:"workout_id"`
+	ID              int      `json:"id"`
+	WorkoutId       int      `json:"workout_id"`
 	ExerciseName    string   `json:"exercise_name"`
 	Description     string   `json:"description"`
 	Sets            int      `json:"sets"`
@@ -44,11 +45,62 @@ type WorkoutStore interface {
 	GetWorkoutById(int int64) (*Workout, error)
 }
 
-func (pgws *PostgresWorkoutStore) CreateWorkout(*Workout) (*Workout, error) {
+func (pgws *PostgresWorkoutStore) CreateWorkout(workout *Workout) (*Workout, error) {
 
-	return nil, nil
+	tx, err := pgws.db.Begin() // begin transaction
+	if err != nil {
+		return nil, err
+	}
+	// we can't commit after rollback.
+	defer tx.Rollback()
+
+	query := `INSERT INTO workouts (title, description, duration_minutes, calories_burned) 
+	VALUES ($1, $2, $3, $4) RETURNING id;`
+
+	err = tx.QueryRow(query, workout.Title, workout.Description, workout.DurationMinutes, workout.CaloriesBurned).Scan(&workout.ID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// we also to do entries
+	for _, entry := range workout.Entries {
+		query := `INSERT INTO workout_entries (workout_id, exercise_name, sets, reps, duration_seconds, weight, notes, order_index) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id;`
+
+		err := tx.QueryRow(query, workout.ID, entry.ExerciseName, entry.Sets, entry.Reps, entry.DurationSeconds, entry.Weight, entry.Notes, entry.OrderIndex).Scan(&entry.ID)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return nil, err
+	}
+
+	return workout, nil
 }
 
-func (pgws *PostgresWorkoutStore) GetWorkoutById(int int64) (*Workout, error) {
-	return nil, nil
+func (pgws *PostgresWorkoutStore) GetWorkoutById(id int64) (*Workout, error) {
+	tx, err := pgws.db.Begin() // begin transaction
+	if err != nil {
+		return nil, err
+	}
+	// we can't commit after rollback.
+	defer tx.Rollback()
+
+	query := `SELECT id, title, description, duration_minutes, calories_burned, entries FROM workouts WHERE id = $1;`
+
+	row := tx.QueryRow(query, id)
+
+	workout := &Workout{}
+	row.Scan(&workout.ID, &workout.Title, &workout.Description, &workout.DurationMinutes, &workout.CaloriesBurned, &workout.Entries)
+	// we also to do entries
+
+	err = tx.Commit()
+	if err != nil {
+		return nil, err
+	}
+
+	return workout, nil
 }
