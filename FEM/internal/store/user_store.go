b37c9e7a8cb3d68a44692ce0008bf4e310c116c5
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2026-04-13 10:43:41 +0530

    L43: User SQL Queries

--- FEM/internal/store/user_store.go
+++ FEM/internal/store/user_store.go
@@ -8,14 +8,14 @@ import (
 
 type password struct {
 	plainText *string
-	hash []byte
+	hash      []byte
 }
 
 type User struct {
 	ID           int       `json:"id"`
 	Username     string    `json:"username"`
 	Email        string    `json:"email"`
-	PasswordHash password    `json:"-"` // - means we are going to ignore the value in the struct
+	PasswordHash password  `json:"-"` // - means we are going to ignore the value in the struct
 	Bio          string    `json:"bio"`
 	CreatedAt    time.Time `json:"created_at"`
 	UpdatedAt    time.Time `json:"updated_at"`
@@ -30,9 +30,51 @@ func NewPostgresUserStore(db *sql.DB, logger *log.Logger) *PostgresUserStore {
 	return &PostgresUserStore{db: db, logger: logger}
 }
 
-type UserStore interface{
+type UserStore interface {
 	CreateUser(*User) error
 	GetUserByUsername(string) (*User, error)
 	UpdateUser(*User) error
 }
 
+func (pgus *PostgresUserStore) CreateUser(user *User) error {
+	query := `INSERT INTO (username, email, password_hash, bio) VALUES ($1, $2, $3, $4) RETURNING id, created_at, updated_at`
+
+	err := pgus.db.QueryRow(query, user.Username, user.Email, user.PasswordHash, user.Bio).Scan(&user.ID, &user.CreatedAt, &user.UpdatedAt)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (pgus *PostgresUserStore) GetUserByUsername(username string) (*User, error) {
+
+	user := &User{
+		PasswordHash: password{},
+	}
+
+	query := `SELECT id, username, email, password_hash, created_at, updated_at FROM users WHERE username = $1`
+
+	err := pgus.db.QueryRow(query, username).Scan(&user.ID, &user.Username, &user.Email, &user.PasswordHash.hash, &user.Bio, &user.CreatedAt, &user.UpdatedAt)
+	if err != nil {
+		return nil, err
+	}
+
+	if err == sql.ErrNoRows {
+		return nil, nil
+	}
+
+	return user, nil
+}
+
+func (pgus *PostgresUserStore) UpdateUser(user *User) error {
+	query := `UPDATE users
+	SET username = $1, email = $2, bio = $3, updated_at = CURRENT_TIMESTAMP WHERE id = $4
+	RETURNING updated_at`
+
+	err := pgus.db.QueryRow(query, user.Username, user.Email, user.Bio, user.ID).Scan(&user.UpdatedAt)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
