b3bea9be66cfc97629ac29a993417298993d434d
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2026-06-01 21:04:51 +0530

    Add constants and enums demonstration in variable declarations

--- basics/variables.go
+++ basics/variables.go
@@ -37,7 +37,29 @@ func main() {
 	var defaultFloat float64
 
 	// this (zeroes values or the default valuews) is way with which compiler assigns us that this is int, string, bool or a float.
-	// cons : sometimes variables must be there, but the value it has undefined or does not exist which is not possible due to the default values.
+	// cons : sometimes variables must be there, but the value it has is undefined or does not exist which is not possible due to the default values.
 
 	fmt.Printf("Default values : Integer: %d, String: %s, Boolean: %t, Float: %f\n", defaultint, defaultString, defaultBool, defaultFloat)
+
+
+	// constant and enums
+	const pi = 3.14 // compiler don't give error if we don't use the constant. it matters with the memory allocation.
+	
+	const (
+		Monday = 1
+		Tuesday = 2
+		Wednesday = 3
+	)
+	
+	fmt.Printf("Monday: %d - Tuesday %d Wednesday %d\n", Monday, Tuesday, Wednesday)
+
+	// Go does not have the enum so there is a way we can have that using const
+	const (
+		Jan int = iota + 1 // 1
+		Feb //2
+		March //3
+		April // 4
+	) // so if the first var is defined using the iota then consecutive values will be incremental with one starting from the current value of the starting variable and iota is ) by default.
+
+	fmt.Printf("Jan : %d, Feb : %d, March: %d, April: %d\n", Jan, Feb, March, April);
 }
