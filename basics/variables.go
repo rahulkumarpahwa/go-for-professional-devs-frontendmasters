1f4812da55bc0e994638e9acaa762173df03b8f7
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2026-06-02 23:09:25 +0530

    Refactor variable declarations and add functions for addition and calculation of sum and product

--- basics/variables.go
+++ basics/variables.go
@@ -41,25 +41,40 @@ func main() {
 
 	fmt.Printf("Default values : Integer: %d, String: %s, Boolean: %t, Float: %f\n", defaultint, defaultString, defaultBool, defaultFloat)
 
-
 	// constant and enums
 	const pi = 3.14 // compiler don't give error if we don't use the constant. it matters with the memory allocation.
-	
+
 	const (
-		Monday = 1
-		Tuesday = 2
+		Monday    = 1
+		Tuesday   = 2
 		Wednesday = 3
 	)
-	
+
 	fmt.Printf("Monday: %d - Tuesday %d Wednesday %d\n", Monday, Tuesday, Wednesday)
 
 	// Go does not have the enum so there is a way we can have that using const
 	const (
-		Jan int = iota + 1 // 1
-		Feb //2
-		March //3
-		April // 4
+		Jan   int = iota + 1 // 1
+		Feb                  //2
+		March                //3
+		April                // 4
 	) // so if the first var is defined using the iota then consecutive values will be incremental with one starting from the current value of the starting variable and iota is ) by default.
 
-	fmt.Printf("Jan : %d, Feb : %d, March: %d, April: %d\n", Jan, Feb, March, April);
+	fmt.Printf("Jan : %d, Feb : %d, March: %d, April: %d\n", Jan, Feb, March, April)
+
+	result := Add(3, 4)
+
+	fmt.Println("Result : ", result)
+
+	sum, prod := CalculateSumAndProduct(3,5);
+	fmt.Printf("Sum and Product is : %d and %d ", sum , prod)
+
+}
+
+func Add(a, b int) int {
+	return a + b
+}
+
+func CalculateSumAndProduct(a, b int) (int, int) {
+	return a + b, a * b
 }
