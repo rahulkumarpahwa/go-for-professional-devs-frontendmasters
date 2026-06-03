1ab93ded5000d0910c1d96cbff475b0a0de6a9ba
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2026-06-03 22:06:35 +0530

    Add initial implementation of arrays and slices demonstration

---
+++ data_structures/datastructures.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Arrays and Slices
+
+	numbers := [5]int{10, 20, 30, 40, 50}
+	fmt.Printf("this is out an array  %v\n", numbers)
+
+	matrix := [2][3]int{
+		{}, {2, 3, 4},
+	}
+	fmt.Printf("this is out a matrix  %v\n", matrix)
+}
