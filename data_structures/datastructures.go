ab4156d6eeca2eadd2b62d25cead3a8a6da36f61
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2026-06-04 23:41:37 +0530

    Enhance slice demonstration with additional fruit appending examples

--- data_structures/datastructures.go
+++ data_structures/datastructures.go
@@ -16,7 +16,24 @@ func main() {
 	// Slices
 	allNumbers := numbers[:]
 	firstThree := numbers[0:3]
-	allNumbers = append(allNumbers, firstThree...)
+	allNumbers = append(allNumbers, firstThree...) // no such method in array
 
-	
+	fmt.Println("The all numbers slice is as : ", allNumbers);
+
+	fruits := []string{"apple", "banana", "mango"}
+	fmt.Printf("these are my fruits: %v\n", fruits);
+
+	// appending single
+	fruits = append(fruits, "kiwi")
+	fmt.Printf("these are my fruits with kiwi: %v\n", fruits);
+
+	// appending multiple
+	fruits = append(fruits, "litchi", "grapes")
+	fmt.Printf("these are my fruits with litchi and grapes: %v\n", fruits);
+
+	moreFruits := []string{"strawberry", "tomato"}
+	fruits = append(fruits, moreFruits...)
+	fmt.Printf("these are my fruits with more fruits: %v\n", fruits);
+
+
 }
